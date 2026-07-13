6d4acf4bd048316110af90d70b4b4a4a09cd55d6
Anurag3215 <anuragaacc@gmail.com>
2026-07-13 23:33:11 +0530

    AddCar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,21 @@ const Navbar = () => {
     return (
         <div>
 
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <Link class="navbar-brand" to="#">Car Rental</Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <Link class="nav-link active" aria-current="page" to="/">Home</Link>
-                            <Link class="nav-link" to="/add">Add Car</Link>
-                            <Link class="nav-link" to="/view">View Cars</Link>
+                    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                        <div className="container-fluid">
+                            <Link className="navbar-brand" to="#">Car Rental</Link>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                <div className="navbar-nav">
+                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                    <Link className="nav-link" to="/add">Add Car</Link>
+                                    <Link className="nav-link" to="/view">View Cars</Link>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </nav>
+                    </nav>
 
         </div>
     )
