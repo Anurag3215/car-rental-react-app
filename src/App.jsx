771a609fbf34a879b04c6d7cfd7455fca55086f0
Sanjaysna <sanjaysanthosh411@gmail.com>
2026-07-14 07:30:58 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { BrowserRouter, Route, Router } from 'react-router-dom'
-import AddCar from './components/AddCar'
+
 import Home from './components/Home'
+import AddCar from './components/AddCar'
 import Viewcars from './components/Viewcars'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home />
-      <AddCar/>
-      <Viewcars />
-
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/add" element={<AddCar/>} />
+          <Route path="/view" element={<Viewcars/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
